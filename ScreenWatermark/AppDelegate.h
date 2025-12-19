03a273158af9b41f76c5b504b17ca4b9e33c383d
Danil Korotenko <danil.korotenko@gmail.com>
2025-12-19 18:19:20 +0200

    Initial commit.

---
+++ ScreenWatermark/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ScreenWatermark
+//
+//  Created by Danil Korotenko on 12/19/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
