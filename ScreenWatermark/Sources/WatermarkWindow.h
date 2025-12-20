24b9752f039c6e01240469d3634f5fc4fe52c224
Danil Korotenko <danil.korotenko@gmail.com>
2025-12-20 19:50:15 +0200

    Watermark window class added.

---
+++ ScreenWatermark/Sources/WatermarkWindow.h
@@ -0,0 +1,16 @@
+//
+//  WatermarkWindow.h
+//  ScreenWatermark
+//
+//  Created by Danil Korotenko on 12/20/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WatermarkWindow : NSWindow
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ScreenWatermark/Sources/WatermarkWindow.m
@@ -0,0 +1,12 @@
+//
+//  WatermarkWindow.m
+//  ScreenWatermark
+//
+//  Created by Danil Korotenko on 12/20/25.
+//
+
+#import "WatermarkWindow.h"
+
+@implementation WatermarkWindow
+
+@end
